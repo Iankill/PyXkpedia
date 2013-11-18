55389c3d7e268b73586621c02630af11b6db8c59
Killian Cazin <killiancazin@gmail.com>
2013-11-18 03:09:53 +0000

    Update to handle bad articles and loops

--- PyXkpedia.py
+++ PyXkpedia.py
@@ -2,14 +2,15 @@ import urllib.request
 import re
 from lxml import html
 
-"""
-Handle loops
-Handle no article pages ("may refer to...") find disambigbox http://en.m.wikipedia.org/wiki/Valley_Lutheran_High_School
 
-"""
+class BadArticleError(Exception): pass
+class LoopingError(Exception): pass
+class NoArticleError(Exception): pass
+
+processed = []
 
 def get_link(link):
-	link = "http://en.m.wikipedia.org/wiki/" + link
+	link = "http://en.wikipedia.org/wiki/" + link
 	fh = urllib.request.urlopen(link)
 	print(fh.geturl())
 	return fh
@@ -17,32 +18,43 @@ def get_link(link):
 def get_parsed_link(fh):
 	return html.parse(fh)
 
-def search(parsed):
 
-	
-	#print(fh.geturl())
+def search(parsed):
 	
 	element_p_pos = 0
 	pos = None
 
+
 	while pos is None:
+		if element_p_pos > 3:
+			raise BadArticleError
 		element_p_pos += 1
-		#print("element_p_pos :" + str(element_p_pos))
-		p = parsed.xpath("//*[@id=\"content\"]/div[1]/p["+str(element_p_pos)+"]/node()")
-		attrib = parsed.xpath("//*[@id=\"content\"]/div[1]/p["+str(element_p_pos)+"]/*")
-		
+		if element_p_pos > 1:
+			print("LOOPING")
+
+		if len(parsed.xpath("//*[@id=\"disambigbox\"]")) > 0: #Disambiguation article
+			p = parsed.xpath("//*[@id=\"mw-content-text\"]/ul/li["+str(element_p_pos)+"]/node()")
+			attrib = parsed.xpath("//*[@id=\"mw-content-text\"]/ul/li["+str(element_p_pos)+"]/*")
+		elif parsed.xpath("//*[@id=\"noarticletext\"]") is True:
+			raise NoArticleError
+		else:
+			p = parsed.xpath("//*[@id=\"mw-content-text\"]/p["+str(element_p_pos)+"]/node()")
+			attrib = parsed.xpath("//*[@id=\"mw-content-text\"]/p["+str(element_p_pos)+"]/*")
+			
 		
 		del_flag = False
 		
 		for i in range(len(p)):
-			if re.search('\(',str(p[i])) is not None:
+			if re.search('\)[^\(]*\(',str(p[i])) is not None:
 				del_flag = True
+				p[i] = None
 			if re.search('\)',str(p[i])) is not None and del_flag:
 				p[i] = None
 				del_flag = False
+			if re.search('\([^\)]*$',str(p[i])) is not None:
+				del_flag = True
 			if del_flag:
 				p[i] = None
-
 
 			
 		p = [el for el in p if el in attrib]
@@ -50,24 +62,42 @@ def search(parsed):
 		for i in range(len(p)):
 			if p[i] != str:
 				if p[i] is not None and p[i].tag == 'a':
-					pos = i
-					break
+					if 'rel' in p[i].attrib:
+						if p[i].attrib['rel'] == 'nofollow': #No follow link
+							p[i] = None
+					else:
+						pos = i
+						break
+
 		if pos is not None:
 			wiki_link = p[pos].attrib['href']
 			next_link = re.sub(r'/wiki/','',wiki_link)
 
-	
-	#Go back if any 'a' in the 'p'
+	if next_link in processed:
+		raise LoopingError
+	else:
+		processed.append(next_link)
 	print(next_link)
-	return next_link
+	return next_link	
+
 
 
 for i in range(10):
 	link = "Special:Random"
-	print("New Test")
+	#link = "Nurettin_Kayao%C4%9Flu"
+	processed = []
+	print("New Test\n")
 	while link != "Philosophy":
-		link = search(get_parsed_link(get_link(link)))
+		try:
+			link = search(get_parsed_link(get_link(link)))
+		except BadArticleError:
+			print("Bad Article Error")
+			break
+		except LoopingError:
+			print("Detected a Loop")
+			break
+		except NoArticleError:
+			print("No article Error")
+			break
+	print()
 
-
-#print(pos)
-#print(p)
